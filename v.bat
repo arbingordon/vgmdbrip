1ca1d1c75ef237e635578f52b32d96ce43d55ede
arbingordon <arbingordon@gmail.com>
2014-11-15 18:45:09 -0500

    first commit

---
+++ v.bat
@@ -0,0 +1,2 @@
+@rem this is a batch file wrapper so you only need to type v album_id
+@C:\Programming\Python33\python C:\Binaries\vgmdbrip\vg.py "%~1"
---
+++ vg.py
@@ -0,0 +1,62 @@
+import urllib.request
+import http.cookiejar
+from sys import argv
+from xml.sax.saxutils import unescape
+import os
+vgmuserid = "xxxx"  # replace this with your user id
+vgmpassword = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # replace this with your vgmpassword (from cookies)
+def remove(instring, chars):
+    for i in range(len(chars)):
+        instring = instring.replace(chars[i],"")
+    return instring
+    
+def substring(string, frm, to):
+    start = string.find(frm) + len(frm)
+    if start == frm:
+        return "NULL"
+    length = string[start:].find(to)
+    if length == -1:
+        return "NULL"
+    return string[start:start+length]
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+if(len(argv) < 2):
+    print("usage: " + argv[0] + " vgmdb_album_id")
+    raise SystemExit(1)
+cp = urllib.request.HTTPCookieProcessor()
+cj = cp.cookiejar
+
+# see cookielib.Cookie documentation for options description
+cj.set_cookie(http.cookiejar.Cookie(0, 'vgmuserid', vgmuserid,
+                               '80', False, '.vgmdb.net', True, False, '/',
+                               True, False, None, False, None, None, None))
+cj.set_cookie(http.cookiejar.Cookie(0, 'vgmpassword', vgmpassword,
+                               '80', False, '.vgmdb.net', True, False, '/',
+                               True, False, None, False, None, None, None))
+opener = urllib.request.build_opener(cp)
+opener.addheaders.append(('User-agent', 'Mozilla/5.0 (compatible)'))
+mainpage = opener.open("http://vgmdb.net/album/" + argv[1]).read().decode(encoding='utf-8', errors="replace").splitlines()
+maintitle = ""
+for i in range(len(mainpage)):
+    if(mainpage[i].find("<h1><span class=\"albumtitle\"") != -1):
+        maintitle = remove(substring(mainpage[i], "\">", "</span>"), "\"*/:<>?\|")
+        print(maintitle)
+        break
+
+for i in range(len(mainpage)):
+    if(mainpage[i].find(">Covers") != -1):
+        i += 8
+        while(mainpage[i].find("/db/") != -1):
+            ID = substring(mainpage[i], "cover=", "\"")
+            title = unescape(substring(mainpage[i], ID + "\">", "</a>"))
+            image = opener.open("http://vgmdb.net/db/covers-full.php?id=" + ID).read()
+            ensure_dir("Scans\\")
+            handle = open("Scans\\" + remove(title, "\"*/:<>?\|") + ".jpg", "wb")
+            handle.write(image)
+            handle.close()
+            print("FOUND cover on line " + str(i+1) + " : ID " + ID + " : title " + title + " -> SAVED!")
+            i += 1
